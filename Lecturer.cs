437c298be665a5f03211bfc338bbf5bf67462c50
st10293982 <128587800+st10293982@users.noreply.github.com>
2024-11-22 13:02:42 +0200

    Add files via upload

---
+++ Lecturer.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Prog6212Part2.Models
+{
+    public class Lecturer
+    {
+        [Key]
+        public string UserId { get; set; } // Matches with Identity User ID
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [Phone]
+        public string ContactNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
